c5a9e590b328aac409cd98f01c2ea241f8d774e4
Saul M <114538686+saulmanz@users.noreply.github.com>
2025-11-16 22:39:33 -0500

    Delete app directory

--- app/(Kambaz)/Calendar/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Calendar() {
-  return (
-    <div id="wd-calendar">
-      <h1 id="wd-calendar-title">Calendar</h1> <hr />
-    </div>
-);}
--- app/(Kambaz)/Inbox/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Inbox() {
-  return (
-    <div id="wd-inbox">
-      <h1 id="wd-inbox-title">Inbox</h1> <hr />
-    </div>
-);}
