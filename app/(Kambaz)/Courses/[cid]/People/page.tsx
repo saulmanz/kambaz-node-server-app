fb53c6607e6ab321d6e54b51b2b628a836ddd894
Saul M <114538686+saulmanz@users.noreply.github.com>
2025-12-01 00:09:35 -0500

    Delete app directory

--- app/(Kambaz)/Courses/[cid]/People/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default async function People({ params, }: { params: Promise<{ cid: string }>; }) {
- const { cid } = await params;
- redirect(`/Courses/${cid}/People/Table`);
-}
--- app/(Kambaz)/page.tsx
+++
@@ -1,4 +0,0 @@
-import { redirect } from "next/navigation";
-export default function Kambaz() {
- redirect("/Account/Signin");
-}
